83331e982e36dcfe0547557ccc5e3af9c9349a6c
AllenHongjun <652971723@163.com>
2020-08-28 20:33:52 +0800

    lab0

--- lab/tmp/HelloWorld.java
+++ lab/tmp/HelloWorld.java
@@ -1,9 +1,5 @@
 public class HelloWorld{
-	//java 里面 变量 属性 一般都是小写
-	// 都当做复习。。安装 jdk .配置 环境变量。。这些事情。。都已经是很熟练的了。
-	// 重要的是如何理解 这个原理。。操作。。其实更加的简单。
 	
-	// javac -encoding utf-8 HelloWorld.java
 	public static void main(String[] args){
 		System.out.println("Hello world!");
 	}
--- skeleton-sp19/lab2setup/Dog.java
+++ skeleton-sp19/lab2setup/Dog.java
@@ -7,6 +7,19 @@ public class Dog {
 
     /** Makes a noise. */
     public String noise() {
+        /**
+         * 树 二叉树 b树 java语言。。ideal 开发工具。。等的使用 jdk github  写出更加有效率的代码。。
+         * 数据接口和算法入门
+         * 算法 1 2 3 4
+         * 1 周内容
+         * 梳理一下学习的方法。。
+         * 先预习
+         * 总体。框架。结构化。
+         *
+         * 一门课可能需要150小时  200小时 250小  300小时
+         * 每一个小时 都是有效的学习时间
+         *
+         */
         if (size < 10) {
             return "yip";
         } 
